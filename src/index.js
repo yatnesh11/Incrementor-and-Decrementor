3845c5a5f2c8f69ebc23d49ea3f789aa6f52744a
Yatnesh <97451459+yatnesh11@users.noreply.github.com>
2024-07-05 10:57:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+// Condition for using hooks you nust use it inside functional components
+
+ReactDOM.render(<App />, document.getElementById("root"));
